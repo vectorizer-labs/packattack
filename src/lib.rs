6d304672b8d748f684c9e91e07905743feac4fbc
Ethan Spurlock <ethan@spurlock.io>
2020-01-07 00:07:06 -0600

    Initital Implementation of FromBitReaderWithLength

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,77 @@
-mod from_bytes;
-mod from_byte;
+pub use {
+    async_trait::async_trait,
+    bitreader_async::BitReader,
+    async_std::io::Read
+};
 
-pub trait FromBytes: Sized
+use bitreader_async::error::*;
+
+#[async_trait]
+pub trait FromBitReader<E, R> 
+    where Self : Sized,
+    R : Read + std::marker::Unpin + std::marker::Send
 {
-    fn read_from_bytes(bytes: &[u8], count : &mut usize) -> Self;
+    async fn from_bitreader(reader : &mut BitReader<R>) -> std::result::Result<Self, E>;
 }
 
-pub trait FromByte: Sized
+#[async_trait]
+pub trait FromBitReaderWithLength<E, R> 
+    where Self : Sized,
+    R : Read + std::marker::Unpin + std::marker::Send
 {
-    fn read_from_byte(byte : &u8, start_index : u8) -> Self;
+    async fn from_bitreader_with_length(reader : &mut BitReader<R>, len : usize) -> std::result::Result<Self, E>;
+}
 
-    const bitmask : u8;
-    const size_in_bits: u8;
+#[async_trait]
+impl<R> FromBitReader<BitReaderError, R> for () where R : Read + std::marker::Unpin + std::marker::Send
+{
+    async fn from_bitreader(_reader : &mut BitReader<R>) -> Result<()>
+    {
+        Ok(())
+    }
+}
+
+#[async_trait]
+impl<R> FromBitReader<BitReaderError, R> for bool where R : Read + std::marker::Unpin + std::marker::Send
+{
+    async fn from_bitreader(reader : &mut BitReader<R>) -> Result<bool>
+    {
+        Ok(reader.read_be_bits(1).await? != 0)
+    }
+}
+
+#[async_trait]
+impl<R> FromBitReader<BitReaderError, R> for u8 where R : Read + std::marker::Unpin + std::marker::Send
+{
+    async fn from_bitreader(reader : &mut BitReader<R>) -> Result<u8>
+    {
+        Ok(reader.read_aligned_be::<u8>().await?)
+    }
+}
+
+#[async_trait]
+impl<R> FromBitReader<BitReaderError, R> for u16 where R : Read + std::marker::Unpin + std::marker::Send
+{
+    async fn from_bitreader(reader : &mut BitReader<R>) -> Result<u16>
+    {
+        Ok(reader.read_aligned_be::<u16>().await?)
+    }
+}
+
+#[async_trait]
+impl<R> FromBitReader<BitReaderError, R> for u32 where R : Read + std::marker::Unpin + std::marker::Send
+{
+    async fn from_bitreader(reader : &mut BitReader<R>) -> Result<u32>
+    {
+        Ok(reader.read_aligned_be::<u32>().await?)
+    }
+}
+
+#[async_trait]
+impl<R> FromBitReaderWithLength<BitReaderError, R> for Vec<u8> where R : Read + std::marker::Unpin + std::marker::Send
+{
+    async fn from_bitreader_with_length(reader : &mut BitReader<R>, len : usize) -> Result<Vec<u8>>
+    {
+        Ok(reader.read_u8_slice_aligned(len).await?)
+    }
 }
